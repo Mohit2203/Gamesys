35d1a21ace10a026136b21349114680ee4ca068a
Mohit <Mohit@DESKTOP-2UJRL6U>
2019-04-04 06:20:54 +0100

    Add Validation

--- src/main/java/com/gamesys/test/GamesysSpringBootUserRegistration/Validator/DateValidatorImpl.java
+++ src/main/java/com/gamesys/test/GamesysSpringBootUserRegistration/Validator/DateValidatorImpl.java
@@ -3,14 +3,11 @@
  */
 package com.gamesys.test.GamesysSpringBootUserRegistration.Validator;
 
-import java.util.Arrays;
-import java.util.Set;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 /**
  * @author Mohit
@@ -18,18 +15,34 @@ import org.springframework.format.annotation.DateTimeFormat.ISO;
  */
 public class DateValidatorImpl implements ConstraintValidator<DateValueMatch, String>{
 
-	private ISO validDaysFormat;
-	
+	private static String isovalidDaysFormat = "yyyy-MM-dd'T'";
 
-    public void initialize(DateTimeFormat constraint) {
-    	validDaysFormat = constraint.iso();
-    }
+	
+	
+	private static String getISO8601StringForDate(Date date) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat(isovalidDaysFormat);
+		return dateFormat.format(date);
+}
+	 
 	@Override
 	public boolean isValid(String value, ConstraintValidatorContext context) {
 		// TODO Auto-generated method stub
 		if (value == null)return false;
+		//Implement The Simple Date Format
+		if(!value.isEmpty()) {
+		}
 		
-		return false;
+		else{
+			try {
+				throw new Exception("Please enter the date in yyyy-MM-dd format");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+		}
+			return true;
+			
+		
 	}
 
 }
