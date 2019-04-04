501ee0a85d85f59a877c46b91e86c556a4b20c79
gaurav89singla@gmail.com <gaurav89singla@gmail.com>
2019-04-04 19:01:16 +0530

    changes

---
+++ src/main/java/com/gamesys/test/GamesysSpringBootUserRegistration/Service/UserBlackListedImpl.java
@@ -0,0 +1,13 @@
+package com.gamesys.test.GamesysSpringBootUserRegistration.Service;
+
+import java.util.List;
+
+public class UserBlackListedImpl implements UserBlackListed {
+
+	@Override
+	public List<String> blackListusers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
