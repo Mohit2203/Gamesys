90e87aa85bcd3161253620d2c5e7c75d02454a63
Mohit <Mohit@DESKTOP-2UJRL6U>
2019-04-04 12:13:31 +0100

    commit

--- src/main/java/com/gamesys/test/GamesysSpringBootUserRegistration/Repository/UserRepository.java
+++ src/main/java/com/gamesys/test/GamesysSpringBootUserRegistration/Repository/UserRepository.java
@@ -6,6 +6,7 @@ import com.gamesys.test.GamesysSpringBootUserRegistration.model.UserDetails;
 
 public interface UserRepository extends JpaRepository<UserDetails, Long>{
 	
+	
 	UserDetails findByUsername(String userName);
 
 }
