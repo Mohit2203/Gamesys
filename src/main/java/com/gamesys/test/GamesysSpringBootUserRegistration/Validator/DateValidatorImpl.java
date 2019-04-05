e6b4b69fc1c197795a8bd4b9e2833c4afa8560d5
Mohit <Mohit@DESKTOP-2UJRL6U>
2019-04-05 07:46:25 +0100

    Update the Code

--- src/main/java/com/gamesys/test/GamesysSpringBootUserRegistration/Validator/DateValidatorImpl.java
+++ src/main/java/com/gamesys/test/GamesysSpringBootUserRegistration/Validator/DateValidatorImpl.java
@@ -1,41 +1,40 @@
 /**
  * 
  */
-package com.gamesys.test.GamesysSpringBootUserRegistration.Validator;
-
-import java.text.SimpleDateFormat;
-
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
-
-import com.gamesys.test.GamesysSpringBootUserRegistration.Constant.UserRegistrationConstant;
-import com.gamesys.test.GamesysSpringBootUserRegistration.Exceptions.DateFormatNotValidException;
-
-/**
- * @author Mohit
- *
- */
-public class DateValidatorImpl implements ConstraintValidator<DateValueMatch, String> {	
-
-	private static String getISO8601StringForDate(String date) {
-		SimpleDateFormat dateFormat = new SimpleDateFormat(UserRegistrationConstant.isovalidDaysFormat);
-		return dateFormat.format(date);
-	}
-
-	@Override
-	public boolean isValid(String value, ConstraintValidatorContext context) {
-		try {
-			if (value == null)
-				throw new DateFormatNotValidException("Please enter the date in yyyy-MM-dd format");
-			String formattedValue = getISO8601StringForDate(value);
-			if (!value.isEmpty() && value.equalsIgnoreCase(formattedValue)) {
-				return true;
-			} else {
-				throw new DateFormatNotValidException("Please enter the date in yyyy-MM-dd format");
-			}
-		} catch (Exception ex) {
-			throw new DateFormatNotValidException("Please enter the date in yyyy-MM-dd format");
-		}
-	}
-
-}
+/*
+ * package com.gamesys.test.GamesysSpringBootUserRegistration.Validator;
+ * 
+ * import java.text.SimpleDateFormat;
+ * 
+ * import javax.validation.ConstraintValidator; import
+ * javax.validation.ConstraintValidatorContext;
+ * 
+ * import com.gamesys.test.GamesysSpringBootUserRegistration.Constant.
+ * UserRegistrationConstant; import
+ * com.gamesys.test.GamesysSpringBootUserRegistration.Exceptions.
+ * DateFormatNotValidException;
+ * 
+ *//**
+	 * @author Mohit
+	 *
+	 *//*
+		 * public class DateValidatorImpl implements ConstraintValidator<DateValueMatch,
+		 * String> {
+		 * 
+		 * private static String getISO8601StringForDate(String date) { SimpleDateFormat
+		 * dateFormat = new
+		 * SimpleDateFormat(UserRegistrationConstant.isovalidDaysFormat); return
+		 * dateFormat.format(date); }
+		 * 
+		 * @Override public boolean isValid(String value, ConstraintValidatorContext
+		 * context) { try { if (value == null) throw new
+		 * DateFormatNotValidException("Please enter the date in yyyy-MM-dd format");
+		 * String formattedValue = getISO8601StringForDate(value); if (!value.isEmpty()
+		 * && value.equalsIgnoreCase(formattedValue)) { return true; } else { throw new
+		 * DateFormatNotValidException("Please enter the date in yyyy-MM-dd format"); }
+		 * } catch (Exception ex) { throw new
+		 * DateFormatNotValidException("Please enter the date in yyyy-MM-dd format"); }
+		 * }
+		 * 
+		 * }
+		 */
--- src/main/java/com/gamesys/test/GamesysSpringBootUserRegistration/Validator/DateValueMatch.java
+++ src/main/java/com/gamesys/test/GamesysSpringBootUserRegistration/Validator/DateValueMatch.java
@@ -1,22 +1,25 @@
-package com.gamesys.test.GamesysSpringBootUserRegistration.Validator;
-
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import javax.validation.Constraint;
-import javax.validation.Payload;
-
-@Documented
-@Target(ElementType.FIELD)
-@Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = DateValidatorImpl.class)
-public @interface DateValueMatch {
-
-	String message() default "{com.gamesys.test.GamesysSpringBootUserRegistration.Validator.DateValueMatch.message}";
-
-	Class<?>[] groups() default {};
-
-	Class<? extends Payload>[] payload() default {};
-}
+/*
+ * package com.gamesys.test.GamesysSpringBootUserRegistration.Validator;
+ * 
+ * import java.lang.annotation.Documented; import
+ * java.lang.annotation.ElementType; import java.lang.annotation.Retention;
+ * import java.lang.annotation.RetentionPolicy; import
+ * java.lang.annotation.Target; import javax.validation.Constraint; import
+ * javax.validation.Payload;
+ * 
+ * @Documented
+ * 
+ * @Target(ElementType.FIELD)
+ * 
+ * @Retention(RetentionPolicy.RUNTIME)
+ * 
+ * @Constraint(validatedBy = DateValidatorImpl.class) public @interface
+ * DateValueMatch {
+ * 
+ * String message() default
+ * "{com.gamesys.test.GamesysSpringBootUserRegistration.Validator.DateValueMatch.message}";
+ * 
+ * Class<?>[] groups() default {};
+ * 
+ * Class<? extends Payload>[] payload() default {}; }
+ */
